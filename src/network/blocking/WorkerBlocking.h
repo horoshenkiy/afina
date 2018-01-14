12fb0c8b607fa694ce450ff1e3253d7b6e1a39b3
horoshenkiy <ilya@ilya-pc.localdomain>
2018-01-14 14:27:33 +0000

    Blocking server

---
+++ src/network/blocking/WorkerBlocking.h
@@ -0,0 +1,8 @@
+//
+// Created by ilya on 1/12/18.
+//
+
+#ifndef AFINA_WORKERIMPL_H_H
+#define AFINA_WORKERIMPL_H_H
+
+#endif //AFINA_WORKERIMPL_H_H
---
+++ test/network/NonBlockingTest.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ilya on 11/14/17.
+//
+
+#include "NonBlockingTest.h"
